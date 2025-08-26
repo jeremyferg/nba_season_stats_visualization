9b5eaa2aeb7a104c93bb72eb35cce8028de26c06
Jeremy Ferguson <jeremyferguson2025@u.northwestern.edu>
2025-08-25 23:34:49 -0500

    Update superset_config.py

--- superset_config.py
+++ superset_config.py
@@ -6,3 +6,8 @@ SECRET_KEY = os.getenv("SUPERSET_SECRET_KEY", "KFe68ZNu5Bw4PiRoIIRP2a5Pt2M")
 FEATURE_FLAGS = {
     "EMBEDDED_SUPERSET": True
 }
+
+SQLALCHEMY_DATABASE_URI = os.environ.get(
+    "SUPERSET_DATABASE_URI",
+    "mysql+pymysql://avnadmin:AVNS_-a_5vz1AV9Ppt95Sozh@mysql-3203b068-nba-stats.k.aivencloud.com:22355/defaultdb?ssl_mode=REQUIRED"
+)
